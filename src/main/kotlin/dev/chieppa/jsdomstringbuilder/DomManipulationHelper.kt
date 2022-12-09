e879f8e8c9e3de3e4b3cab2b5c1c9fb1782e9e59
Nicholas Chieppa <nrc4867@rit.edu>
2022-12-09 06:57:51 -0500

    modify helper methods

--- src/main/kotlin/dev/chieppa/jsdomstringbuilder/DomManipulationHelper.kt
+++ src/main/kotlin/dev/chieppa/jsdomstringbuilder/DomManipulationHelper.kt
@@ -4,12 +4,17 @@ object DomManipulationHelper {
 
     fun forEachIndexed(iteratorName: String, listReference: String, methodBody: String) : String =
 """
-for(let $iteratorName = 0; $iteratorName < $listReference.length; $iteratorName++) { $methodBody }
+for(let $iteratorName = 0; $iteratorName < $listReference.length; $iteratorName++) { $methodBody } 
 """.trimIndent()
 
     fun wrapCommands(commands: String) =
 """
-(() => { $commands })()
+(() => { $commands })(); 
+""".trimIndent()
+
+    fun ifHelper(condition: String, body: String, elseBody: String = "") =
+"""
+if($condition) { $body } else { $elseBody } 
 """.trimIndent()
 
 }
